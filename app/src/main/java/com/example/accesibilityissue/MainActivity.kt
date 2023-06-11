fef87ecda795166f6f4f56350e516af7bdfabec2
Adrián Lázaro <adri92css@gmail.com>
2023-06-11 19:48:10 +0200

    Comments

--- app/src/main/java/com/example/accesibilityissue/MainActivity.kt
+++ app/src/main/java/com/example/accesibilityissue/MainActivity.kt
@@ -12,18 +12,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.semantics.invisibleToUser
-import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.accesibilityissue.ui.theme.AccesibilityIssueTheme
 import com.example.unmodifiablemodule.ContentTextUnmodifiable
 
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -34,23 +31,27 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.fillMaxSize().semantics(true) {
-                            //We want to merge all the descendants and avoid the focus of accesibility services.
-                            //However, by this way, we are losing useful semantic properties of Texts of ContentTextUnmodifiable,
-                            //like testTag.
-                            invisibleToUser()
-                        }
+                        modifier = Modifier.fillMaxSize()
                         ) {
                         ContentTextUnmodifiable(
                             title = "Title",
-                            description = "Description"
+                            description = "Description",
+                            //We want to avoid the focus of accessibility services for child elements.
+                            //However, by this way, we are losing useful semantic properties of Texts of ContentTextUnmodifiable,
+                            //like testTag, and we shouldn't set that property here because potentially "ContentTextUnmodifiable"
+                            //can be used from other Composables.
+                            //Usually, method invisibleToUser() can be invoked for avoid focus too, but
+                            //in the same way we're losing useful children semantic properties like testTag.
+                            //Modifier's "focusable" is not working either for complex views.
+                            modifier = Modifier.clearAndSetSemantics { }
                         )
 
                         Spacer(modifier = Modifier.weight(1f))
 
                         Checkbox(
+                            //useless checkbox
                             checked = true, onCheckedChange = { },
-                            modifier = Modifier.testTag("CHECKBOX")
+                            modifier = Modifier.testTag("checkbox")
                         )
 
                     }
